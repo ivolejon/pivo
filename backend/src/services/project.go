9b661ad769c02e1b5a0ab63f9c8427cb0f203f7f
Ivo <ivo.lejon@gmail.com>
2025-03-11 00:11:29 +0100

    chore: clean up, update prompt

--- backend/src/services/project.go
+++ backend/src/services/project.go
@@ -87,6 +87,7 @@ func (c *ProjectService) Query(question string) (*string, error) {
 	1.  **Prioritize Context:** Carefully analyze the information provided in the given context. Treat this context as your primary source of truth. If the context is insufficient, you can use the LLM to fill in the gaps.**
 	2. **Be Concise:** Provide a clear and concise response to the user query. Avoid unnecessary information or verbosity.**
 	3. **Be Accurate:** Never refer to the context or the LLM in your answer.**
+	4. **This is super important: Your output and answers should be json format, in this format, [{"title": string, "content": string}] for the different sections.**
 
 	**In essence, context first, LLM second.**
 	This is the context: {{.input_documents}}\n\n
@@ -97,21 +98,12 @@ func (c *ProjectService) Query(question string) (*string, error) {
 		[]string{"input_documents", "question"},
 	)
 	combineChain := chains.NewStuffDocuments(chains.NewLLMChain(c.llm, prompt))
-	r := c.vectorStore.Retriver(2)
-	chain := chains.NewRetrievalQA(combineChain, r)
-	// systemPrompt := prompts.NewSystemMessagePromptTemplate(systemTemplate, []string{})
-	// combinedStuffQAChain := LoadStuffQA(llm)
-	// chain := chains.NewConversationalRetrievalQAFromLLM(c.llm,
-	// 	c.vectorStore.Provider.GetRetriver(1), memory.NewConversationBuffer())
+	retriever := c.vectorStore.Retriver(2)
+	chain := chains.NewRetrievalQA(combineChain, retriever)
 
 	res, err := chains.Run(context.Background(), chain, question)
 	if err != nil {
 		return nil, err
 	}
 	return &res, nil
-	// propmt := prompts.ChatPromptTemplate{
-	// 	Template: `Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.``
-
-	// }
-	// chains.New
 }
