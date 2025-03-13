9ffa15298b3eea4d51b9fcfb18fd4da38b776b39
Ivo <ivo.lejon@gmail.com>
2025-03-13 15:15:50 +0100

    fix: prompts update

--- backend/src/chains/base_document_chain.go
+++ backend/src/chains/base_document_chain.go
@@ -6,13 +6,13 @@ func getBaseDocumentChainSystemPrompt() string {
 
 		**Process:**
 
-		1.  **Prioritize Context:** Carefully analyze the information provided in the given context. Treat this context as your primary source of truth. If the context is insufficient, you can use the LLM to fill in the gaps.**
+		1.  **Prioritize Knowledgebase:** Carefully analyze the information provided in the given knowledgebase. Treat this knowledgebase as your primary source of truth. If the knowledgebase is insufficient, you can use the LLM to fill in the gaps.**
 		2. **Be Concise:** Provide a clear and concise response to the user query. Avoid unnecessary information or verbosity.**
-		3. **Never refer to the context or the LLM in your answer.**
+		3. **Never refer to the knowledgebase or the LLM in your answer.**
 		4. **Never make stuff up, just output text you are sure about.**
 
-		**In essence, context first, LLM second.**
-		This is the context: {{.input_documents}}\n\n
+		**In essence, knowledgebase first, LLM second.**
+		This is the knowledgebase: {{.input_documents}}\n\n
 		And this is the question: {{.question}}`
 }
 
@@ -21,3 +21,21 @@ func getFormatAsDocumentChain() string {
 	**Process:**
 	I want you do format the text as a document with headings, preambles, and paragraphs, in markdown. This is the text: {{.output}}`
 }
+
+// func getRefineParagraphChain(userContext string) string {
+// 	return fmt.Sprintf(`
+// 		You are an expert information retrieval and synthesis agent. Your primary task is to answer user queries accurately and comprehensively.
+
+// 		**Process:**
+
+// 		1.  **Prioritize Knowledgebase:** Carefully analyze the information provided in the given knowledgebase. Treat this knowledgebase as your primary source of truth. If the knowledgebase is insufficient, you can use the LLM to fill in the gaps.**
+// 		2. **Never refer to the knowledgebase or the LLM in your answer.**
+// 		3. **Never make stuff up, just output text you are sure about.**
+// 		4. **The user will ask you to work with **
+
+// 		**In essence, knowledgebase first, LLM second.**
+// 		This is the knowledgebase: {{.input_documents}}\n\n
+// 		This is the user context you will work with: %s\n\n
+// 		And this is the question from the user: {{.question}}
+// 		`, userContext)
+// }
