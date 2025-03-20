3a9996bb03b3b5239a7ddc4a2c0791913aa7afc4
Ivo <ivo.lejon@gmail.com>
2025-03-20 21:25:25 +0100

    chore: added tests

---
+++ backend/src/services/upload/upload_test.go
@@ -0,0 +1,33 @@
+package upload_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/ivolejon/pivo/services/document_loader"
+	"github.com/ivolejon/pivo/services/upload"
+	"github.com/stretchr/testify/require"
+)
+
+var clientID = uuid.MustParse("0ef8a743-f92b-4280-937b-ef1e4736c626")
+
+func TestUploadServiceNew(t *testing.T) {
+	_, err := upload.NewUploadService(clientID)
+	require.NoError(t, err)
+}
+
+func TestUploadServiceSave(t *testing.T) {
+	svc, err := upload.NewUploadService(clientID)
+	require.NoError(t, err)
+	IDs, err := svc.Save(upload.UploadFileParams{Data: []byte("test"), Filename: "test.txt"})
+	require.NoError(t, err)
+	require.NotNil(t, IDs)
+}
+
+func TestUploadServiceSaveWrongFileType(t *testing.T) {
+	svc, err := upload.NewUploadService(clientID)
+	require.NoError(t, err)
+	IDs, err := svc.Save(upload.UploadFileParams{Data: []byte("test"), Filename: "test.doccc"})
+	require.ErrorIs(t, err, document_loader.ErrFileTypeNotSupported)
+	require.Nil(t, IDs)
+}
