ffab74f53f4d85a6099720b73bfbbca2bfc0d043
Ivo <ivo.lejon@gmail.com>
2025-03-22 13:56:24 +0100

    wip: frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,37 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Theme, Button } from "@radix-ui/themes";
-import './App.css'
+import { Button, Card } from "@radix-ui/themes";
+import { Chat } from './components/Chat/Chat'
+import './styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <div className='app-container'>
+        <div className='top-bar-container'>ivo</div>
+        <div className='left-menu-container'>Menu</div>
+        <div className='main-container'>
+          <div className="left">
+            <Button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </Button>
+            <Chat />
+          </div>
+
+        </div>
+      </div >
+      {/* <h1>Vite + React</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
---
+++ frontend/src/components/Chat/Chat.tsx
@@ -0,0 +1,9 @@
+import './Chat.css'
+
+export function Chat() {
+  return (
+    <div className='chat-container'>
+      <div className='chat-header' contentEditable={true}>Chat</div>
+    </div>
+  )
+}
